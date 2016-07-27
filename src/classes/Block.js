497983d7aece11b92b8b084619b077ae0b063ffd
Eduardo Ojeda <eduardo.ojeda@researchgate.net>
2016-07-27 23:44:02 +0200

    Blocks now fall when collided

--- src/classes/Block.js
+++ src/classes/Block.js
@@ -2,7 +2,7 @@ class Block extends aggregation(Mass, Collidable) {
     constructor(x, y, pinned) {
         super(x, y)
         this.pos = new Vector(x, y)
-        this.mass = 0
+        this.mass = 1
         this.pinned = pinned
         this.hitArea = new SAT.Box(new SAT.Vector(x, y), 50, 20).toPolygon()
         this.color = "rgb(0,200,0)"
@@ -25,7 +25,7 @@ class Block extends aggregation(Mass, Collidable) {
 
     collide(entity, collision) {
         this.color = "rgb(200,0,0)"
-        //this.pinned = false
+        this.pinned = false
     }
 
     getHitArea() {
